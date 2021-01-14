7ff7798635c3d5b083c5a00fdc2cc983fadf35ac
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-15 01:45:06 +0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,28 @@
+import argparse
+
+parser = argparse.ArgumentParser('Args for PGGAN')
+parser.add_argument('--data_root', type=str, default='data/celeba_faces')
+parser.add_argument('--use_ema', type=bool, default=True)       # whether to use EMA to weights of generator.
+parser.add_argument('--ema_mu', type=float, default=0.999)      # smoothing factor for smoothed generator.
+parser.add_argument('--latent_dim', type=int, default=512)      # input dimension of noise.
+parser.add_argument('--use_cuda', type=bool, default=True)      # whether to use cuda device.
+parser.add_argument('--switch_mode_number', type=int, default=800000)  # number of passed images to switch mode.
+
+
+parser.add_argument('--use_weightscale', type=bool, default=True)  # whether to use equalized-learning rate.
+parser.add_argument('--use_pixelnorm', type=bool, default=True)    # whether to use pixelwise normalization for generator.
+parser.add_argument('--use_gdrop', type=bool, default=False)        # whether to use generalized dropout layer for discriminator.
+parser.add_argument('--use_leaky', type=bool, default=True)         # whether to use leaky relu instead of relu.
+parser.add_argument('--tanh_at_end', type=bool, default=False)     # whether to use tanh at the end of the generator.
+parser.add_argument('--sigmoid_at_end', type=bool, default=False)  # whether to use sigmoid at the end of the discriminator.
+parser.add_argument('--minibatch_stat_concat', type=bool, default=True)  # whether to add minbatch-std-channel in the discriminator.
+parser.add_argument('--normalize_latent', type=bool, default=True)  # whether to use pixelwise normalization of latent vector.
+
+
+parser.add_argument('--lr', type=float, default=0.001)              # learning rate.
+parser.add_argument('--lr_decay', type=float, default=0.87)         # learning rate decay at every resolution transition.
+parser.add_argument('--beta0', type=float, default=0.0)             # beta0 for adam.
+parser.add_argument('--beta1', type=float, default=0.99)            # beta1 for adam.
+
+
+config, _ = parser.parse_known_args()
