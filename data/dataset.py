b89ca8f72a9a43028cfdef0132b2d7b2a77d925f
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-15 02:21:03 +0800

    Update dataset.py

--- data/dataset.py
+++ data/dataset.py
@@ -3,24 +3,29 @@ import os
 from PIL import Image
 import numpy as np
 import torch
+import math
 
 
 class CelebA(object):
-    def __init__(self, transform=None):
-        self.database = 'data/celeba_faces'
+    def __init__(self, data_root='data/celeba_faces', transform=None, max_resolution=256):
+        self.data_root = data_root
         self.images = []
         self.reload_images()
         self._length = len(self.images)
         self.transform = transform
+        self.max_resolution = max_resolution
+        self.R = int(math.log2(max_resolution))
+        assert 2 ** self.R == max_resolution
+        assert str(max_resolution) in os.listdir(self.data_root)
 
     def reload_images(self):
-        self.images = os.listdir(f'{self.database}/4/data')
+        self.images = os.listdir(f'{self.data_root}/4/data')
         random.shuffle(self.images)
 
     def __call__(self, batch_size, level):
-        assert level in range(2, 9)
+        # assert level in range(2, self.R + 1)
         resolution = 2 ** level
-        dir_path = os.path.join(self.database, str(resolution), 'data')
+        dir_path = os.path.join(self.data_root, str(resolution), 'data')
         if len(self.images) < batch_size:
             self.reload_images()
         img_files, self.images = self.images[:batch_size], self.images[batch_size:]
