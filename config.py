9d7b23f769d19a339a1030d52ade9ca5ec7332b9
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-15 01:52:06 +0800

    Update config.py

--- config.py
+++ config.py
@@ -7,12 +7,13 @@ parser.add_argument('--ema_mu', type=float, default=0.999)      # smoothing fact
 parser.add_argument('--latent_dim', type=int, default=512)      # input dimension of noise.
 parser.add_argument('--use_cuda', type=bool, default=True)      # whether to use cuda device.
 parser.add_argument('--switch_mode_number', type=int, default=800000)  # number of passed images to switch mode.
-
-
+parser.add_argument('--n_critic', type=int, default=1)           # n(D)/n(G)
+parser.add_argument('--max_feature_map', type=int, default=256)   # max number of feature maps of Convolution layers.
 parser.add_argument('--use_weightscale', type=bool, default=True)  # whether to use equalized-learning rate.
 parser.add_argument('--use_pixelnorm', type=bool, default=True)    # whether to use pixelwise normalization for generator.
 parser.add_argument('--use_gdrop', type=bool, default=False)        # whether to use generalized dropout layer for discriminator.
 parser.add_argument('--use_leaky', type=bool, default=True)         # whether to use leaky relu instead of relu.
+parser.add_argument('--negative_slope', type=float, default=0.2)   # negative slope for leaky relu
 parser.add_argument('--tanh_at_end', type=bool, default=False)     # whether to use tanh at the end of the generator.
 parser.add_argument('--sigmoid_at_end', type=bool, default=False)  # whether to use sigmoid at the end of the discriminator.
 parser.add_argument('--minibatch_stat_concat', type=bool, default=True)  # whether to add minbatch-std-channel in the discriminator.
