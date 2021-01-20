760966dba13fc69f94d1c17dc7b5c8d08163f4cd
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-20 20:54:45 +0800

    Update preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -4,10 +4,10 @@ import tqdm
 
 
 if __name__ == '__main__':
-    total_images = os.listdir('celeba_faces/256/data')
-    for image_path in tqdm.tqdm(total_images[147000:]):
-        img = Image.open(os.path.join('celeba_faces/256/data', image_path))
+    total_images = os.listdir('flickr/256/data')
+    for image_path in tqdm.tqdm(total_images):
+        img = Image.open(os.path.join('flickr/256/data', image_path))
         for resolution in [4, 8, 16, 32, 64, 128]:
-            save_path = os.path.join('celeba_faces', str(resolution), 'data', image_path)
+            save_path = os.path.join('flickr', str(resolution), 'data', image_path)
             img.resize(size=(resolution, resolution)).save(save_path)
         img.close()
