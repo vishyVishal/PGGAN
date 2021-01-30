0c1f3d7b7cc1f45c4802cbe4f264d2973f832c5f
windshadow233 <50669888+windshadow233@users.noreply.github.com>
2021-01-30 17:04:06 +0800

    Update preprocess.py

--- data/preprocess.py
+++ data/preprocess.py
@@ -1,13 +1,19 @@
 from PIL import Image
 import os
 import tqdm
+import argparse
 
 
+parser = argparse.ArgumentParser('Choose Data Root')
+parser.add_argument('--data_root', '-dr', type=str, default='celeba_faces')
+args, _ = parser.parse_known_args()
 if __name__ == '__main__':
-    total_images = os.listdir('flickr/256/data')
+    total_images = os.listdir(os.path.join(args.data_root, '256/data'))
     for image_path in tqdm.tqdm(total_images):
-        img = Image.open(os.path.join('flickr/256/data', image_path))
+        img = Image.open(os.path.join(args.data_root, '256/data', image_path))
         for resolution in [4, 8, 16, 32, 64, 128]:
-            save_path = os.path.join('flickr', str(resolution), 'data', image_path)
+            if not os.path.isdir(os.path.join(args.data_root, str(resolution), 'data')):
+                os.mkdir(os.path.join(args.data_root, str(resolution), 'data'))
+            save_path = os.path.join(args.data_root, str(resolution), 'data', image_path)
             img.resize(size=(resolution, resolution)).save(save_path)
         img.close()
